ecc57404036aee72267937678bee3d6b5747bb5c
MELDemy <mahmoudeldemerdash5@gmail.com>
2024-02-27 18:59:17 +0200

    initiate Task3

---
+++ task3/lib/Home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CHAT"),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
